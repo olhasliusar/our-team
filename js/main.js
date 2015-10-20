ed705a1d07f77581c314ae33ed66b47ea3e10bba
olhasliusar <olha.sliusar@gmail.com>
2015-10-20 23:23:01 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-;(function(){
+;(function($){
+	$(document).ready(function(){
+		var $slider = $('.ba-team-slider'),
+			$prev = $('.slick-prev', $slider),
+			$next = $('.slick-next', $slider);
 
-})();
+		//slider init
+		$slider.slick({
+			dots: true,
+			prevArrow: $prev,
+			nextArrow: $next,
+			slide: '.ba-crewman__slide'
+		});
+	});
+
+})(jQuery);
+
